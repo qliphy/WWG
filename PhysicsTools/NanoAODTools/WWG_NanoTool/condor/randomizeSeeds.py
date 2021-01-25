22cd43fe14ac713a55a9d43b1e431862dfae6673
phy-guanzh <guanzh97@163.com>
2021-01-25 08:07:17 +0800

    simplified

--- PhysicsTools/NanoAODTools/WWG_NanoTool/condor/randomizeSeeds.py
+++
@@ -1,5 +0,0 @@
-def randomizeSeeds(process):
-    from IOMC.RandomEngine.RandomServiceHelper import RandomNumberServiceHelper
-    rndSvc=RandomNumberServiceHelper(process.RandomNumberGeneratorService)
-    rndSvc.populate()
-    return process
