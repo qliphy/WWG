56698621fdf8a304393c9958c7c2fea468df3ca7
phy-guanzh <guanzh97@163.com>
2021-03-03 00:46:19 +0800

    fix tack bug

--- WWG_selector/WWG_Module.py
+++ WWG_selector/WWG_Module.py
@@ -177,7 +177,7 @@ class WWG_Producer(Module):
     #else:
     #    n_minus +=1
 
-        if not ((event.HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL or event.HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TackIdL_IsoVL) or (event.HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL) or (event.HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8)):
+        if not ((event.HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL or event.HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL) or (event.HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL) or (event.HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8)):
             return False
 
         if  event.MET_pt>20:
