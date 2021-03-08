3d40a74893540ad80775752bfe0af345bcf28a6b
phy-guanzh <guanzh97@163.com>
2021-03-08 17:01:46 +0800

    TMVA

--- crab/dataset_2018_data_nano_v7.py
+++ crab/dataset_2018_data_nano_v7.py
@@ -1,21 +1,21 @@
 Samples = {}
 # data A B C D
-Samples["DoubleMuon_Run2018A"]      = {'nanoAOD': '/DoubleMuon/Run2018A-Nano1June2019-v1/NANOAOD'}
-Samples["DoubleMuon_Run2018B"]      = {'nanoAOD': '/DoubleMuon/Run2018B-Nano1June2019-v1/NANOAOD'}
-Samples["DoubleMuon_Run2018C"]      = {'nanoAOD': '/DoubleMuon/Run2018C-Nano1June2019-v1/NANOAOD'}
-Samples["DoubleMuon_Run2018D"]      = {'nanoAOD': '/DoubleMuon/Run2018D-Nano1June2019_ver2-v1/NANOAOD'}
+Samples["DoubleMuon_Run2018A"]      = {'nanoAOD': '/DoubleMuon/Run2018A-02Apr2020-v1/NANOAOD'}
+Samples["DoubleMuon_Run2018B"]      = {'nanoAOD': '/DoubleMuon/Run2018B-02Apr2020-v1/NANOAOD'}
+Samples["DoubleMuon_Run2018C"]      = {'nanoAOD': '/DoubleMuon/Run2018C-02Apr2020-v1/NANOAOD'}
+Samples["DoubleMuon_Run2018D"]      = {'nanoAOD': '/DoubleMuon/Run2018D-02Apr2020-v1/NANOAOD'}
 
-Samples["EGamma_Run2018A"]      = {'nanoAOD': '/EGamma/Run2018A-Nano1June2019-v1/NANOAOD'}
-Samples["EGamma_Run2018B"]      = {'nanoAOD': '/EGamma/Run2018B-Nano1June2019-v1/NANOAOD'}
-Samples["EGamma_Run2018C"]      = {'nanoAOD': '/EGamma/Run2018C-Nano1June2019-v1/NANOAOD'}
-Samples["EGamma_Run2018D"]      = {'nanoAOD': '/EGamma/Run2018D-Nano1June2019-v1/NANOAOD'}
+Samples["EGamma_Run2018A"]      = {'nanoAOD': '/EGamma/Run2018A-02Apr2020-v1/NANOAOD'}
+Samples["EGamma_Run2018B"]      = {'nanoAOD': '/EGamma/Run2018B-02Apr2020-v1/NANOAOD'}
+Samples["EGamma_Run2018C"]      = {'nanoAOD': '/EGamma/Run2018C-02Apr2020-v1/NANOAOD'}
+Samples["EGamma_Run2018D"]      = {'nanoAOD': '/EGamma/Run2018D-02Apr2020-v1/NANOAOD'}
 
-Samples["SingleMuon_Run2018A"]      = {'nanoAOD': '/SingleMuon/Run2018A-Nano1June2019-v1/NANOAOD'}
-Samples["SingleMuon_Run2018B"]      = {'nanoAOD': '/SingleMuon/Run2018B-Nano1June2019-v1/NANOAOD'}
-Samples["SingleMuon_Run2018C"]      = {'nanoAOD': '/SingleMuon/Run2018C-Nano1June2019-v1/NANOAOD'}
-Samples["SingleMuon_Run2018D"]      = {'nanoAOD': '/SingleMuon/Run2018D-Nano1June2019-v1/NANOAOD'}
+Samples["SingleMuon_Run2018A"]      = {'nanoAOD': '/SingleMuon/Run2018A-02Apr2020-v1/NANOAOD'}
+Samples["SingleMuon_Run2018B"]      = {'nanoAOD': '/SingleMuon/Run2018B-02Apr2020-v1/NANOAOD'}
+Samples["SingleMuon_Run2018C"]      = {'nanoAOD': '/SingleMuon/Run2018C-02Apr2020-v1/NANOAOD'}
+Samples["SingleMuon_Run2018D"]      = {'nanoAOD': '/SingleMuon/Run2018D-02Apr2020-v1/NANOAOD'}
 
-Samples["MuonEG_Run2018A"]      = {'nanoAOD': '/MuonEG/Run2018A-Nano1June2019-v1/NANOAOD'}
-Samples["MuonEG_Run2018B"]      = {'nanoAOD': '/MuonEG/Run2018B-Nano1June2019-v1/NANOAOD'}
-Samples["MuonEG_Run2018C"]      = {'nanoAOD': '/MuonEG/Run2018C-Nano1June2019-v1/NANOAOD'}
-Samples["MuonEG_Run2018D"]      = {'nanoAOD': '/MuonEG/Run2018D-Nano1June2019_ver2-v1/NANOAOD'}
+Samples["MuonEG_Run2018A"]      = {'nanoAOD': '/MuonEG/Run2018A-02Apr2020-v1/NANOAOD'}
+Samples["MuonEG_Run2018B"]      = {'nanoAOD': '/MuonEG/Run2018B-02Apr2020-v1/NANOAOD'}
+Samples["MuonEG_Run2018C"]      = {'nanoAOD': '/MuonEG/Run2018C-02Apr2020-v1/NANOAOD'}
+Samples["MuonEG_Run2018D"]      = {'nanoAOD': '/MuonEG/Run2018D-02Apr2020-v1/NANOAOD'}
